b215e287d0f88fecbbfe1de0d1f2791b47919d55
lgulich <lgulich@ethz.ch>
2018-12-04 20:23:03 +0100

    add path set point marker

--- catkin_ws/src/50-misc-additional-functionality/obst-avoid/obst_avoid_viz/src/path_viz_node.py
+++ catkin_ws/src/50-misc-additional-functionality/obst-avoid/obst_avoid_viz/src/path_viz_node.py
@@ -1,52 +1,15 @@
 #!/usr/bin/env python
+
 import rospy
-
-from nav_msgs.msg import Path
-from duckietown_msgs.msg import TimedPath
-from geometry_msgs.msg import PoseStamped
-
-
-class PathViz:
-    """A class for visualization of the planned path"""
-    def __init__(self):
-        self.timed_path_sub = rospy.Subscriber('obst_avoid/trajectory', TimedPath, self.timedPathSubCb)
-        self.path_pub = rospy.Publisher('obst_avoid_viz/path', Path, queue_size=10)
-
-    def __del__(self):
-        pass
-
-    def timedPathSubCb(self, data):
-
-        path = Path()
-        path.header.stamp = data.start_time
-        path.header.frame_id = 'map'
-        for i, elem in enumerate(data.positions):
-            pose = PoseStamped()
-            pose.header.stamp = data.start_time + rospy.Duration(data.times[i]*data.duration)
-            pose.header.frame_id = 'map'
-            pose.pose.position.x = data.positions[i].x
-            pose.pose.position.y = data.positions[i].y
-            pose.pose.position.z = 0
-            pose.pose.orientation.x = 0
-            pose.pose.orientation.y = 0
-            pose.pose.orientation.z = 0
-            pose.pose.orientation.w = 0
-
-            path.poses.append(pose)
-        self.path_pub.publish(path)
-
-        # trajectory = Trajectory()
-        # trajectory.fromMsg(data)
-        # x,y = trajectory.getPositionFromTimePoint(rospy.Time.now())
-        # marker_msg = Marker()
-        # marker_msg.header.stamp = d
-        # marker_msg.header.frame_id = 'map'
+from obst_avoid_viz import PathViz
 
 
 def main():
-    rospy.init_node('path_node')
-    path_viz = PathViz()
-    rospy.spin()
+    rospy.init_node('path_viz_node', anonymous=False)
+
+    # instantiate standalone worker with 20 hz
+    path_viz = PathViz(standalone=True, frequency=20)
+
 
 if __name__ == '__main__':
     main()

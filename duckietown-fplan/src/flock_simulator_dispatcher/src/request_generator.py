c9955f699040e0cbc7c9655fbf967772573faf1d
lgulich <lgulich@ethz.ch>
2018-12-14 19:49:26 +0100

    restructuring

--- duckietown-fplan/src/flock_simulator_dispatcher/src/request_generator.py
+++
@@ -1,8 +0,0 @@
-def request_generator(maps):
-    open_requests = {}
-
-    # TODO Generate Requests
-    # start_loc = open_request['startlocation']
-    # end_loc = open_request['endlocation']
-
-    return open_requests
